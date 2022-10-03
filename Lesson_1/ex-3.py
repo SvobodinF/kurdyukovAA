313f3f6e13c4bf78232e44f81e20e81ed99a02b3
SvobodinF <artemkurdhy@gmail.com>
2022-10-03 16:33:03 +0300

    [Fix] Ex-3

--- Lesson_1/ex-3.py
+++ Lesson_1/ex-3.py
@@ -1,18 +1,28 @@
-age = int(input("Введите возраст"))
 
-if age > 0 & age < 75:
-    print("Правильный возраст")
-else:
-    print("Неправильный возраст")
 
-if age >= 16:
-    print("Поздравляем вы поступили в ВГУИТ")
-else:    
-    print("Сначала нужно окончить школу!")
-    a = 16 - age
-    print("вам на пары через: ", a)
+def func():
+    age = int(input("Введите возраст"))
 
-name = input("Введите имя")
+    if age > 0 and age < 75:
+        print("Правильный возраст")
+    else:
+        print("Неправильный возраст")
+        return
 
-if name == "Иван":
-    print("ВЫ ИВАН") 
+    if age >= 16:
+        print("Поздравляем вы поступили в ВГУИТ")
+    else:    
+        print("Сначала нужно окончить школу!")
+        a = 16 - age
+        print("вам на пары через: ", a)
+
+        return
+
+    name = input("Введите имя")
+
+    if name == "Иван":
+        print("ВЫ ИВАН") 
+        return
+
+
+func()
